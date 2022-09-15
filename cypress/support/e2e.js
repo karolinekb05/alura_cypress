3497d9d39f29b11a40a971533f3dd39027e3152b
Karoline KB <79204560+karolinekb05@users.noreply.github.com>
2022-09-15 09:24:10 -0300

    new fixture and commands

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,9 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import './gui_command'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+import 'cypress-mochawesome-reporter/register';
